f6ef0f7046996d9bcedc99b77c9cf008ecbf172f
Gavin Douglas <gavin@kronos.pharmacology.dal.ca>
2016-09-04 18:36:48 -0300

    updated trimming and merging input cmds

--- bbmap_fastq_trim_commands.pl
+++ bbmap_fastq_trim_commands.pl
@@ -1,45 +1,82 @@
 use strict;
 use warnings;
 
-my $lc = 0;
+if ( scalar @ARGV != 4 )	{
 
-my %full = ();
+	die "usage: perl bbmap_fastq_trim_commands.pl (directory containing all deconvoluted plates) (keyfile) (out directory) (path to bbduk.sh - bbmap bash script)\n";
+
+}
+
+my $in_dir = $ARGV[0];
+my $key = $ARGV[1];
+my $out_dir = $ARGV[2];
+my $bbmap = $ARGV[3];
+
+my $lc = 0; #line counter
+
+my %full = (); ### full cutsite for each enzyme
 $full{"ApeKI"} = "GCWGC";
 $full{"PstI/EcoT22I"} = "KTGCAK";
 
-my %plate2enzyme = ();
+my %info2enzyme = (); ### hash with deconvoluted fastq folder names as keys and enzymes as values
 
-open ( 'KEY' , '<../Tassel5_realign/keyfiles/20151116_gbs_keyfile_usda_abc_cet_barcode_splitter.txt' ) or die "can't open KEY 20151116_gbs_keyfile_usda_abc_cet_barcode_splitter.txt\n";
+my %indices = (); ### indices of flowcell, lane, plate and enzyme
+$indices{"Flowcell"} = undef;
+$indices{"LibraryPlate"} = undef;
+$indices{"Lane"} = undef;
+$indices{"Enzyme"} = undef;
+
+### read in keyfile and figure out indices of the 4 columns above
+### then link sequencing info with enzyme
+open ( 'KEY' , '<' , $key ) or die "cant open KEY $key\n";
 while( <KEY> )	{
 		
 	my @s = split( '[\t\n]' , $_ );
 	
 	if ( $lc == 0 )	{
 		
+		my $i = 0;
+		foreach my $s ( @s )	{
+			if ( exists $indices{$s} )	{
+				$indices{$s} = $i;
+			}
+			++$i;
+		}
+		
 		++$lc; 
 		next;	
 	}	
 	
-	$plate2enzyme{$s[4]} = $s[9];
+	my $flowcell = $s[ $indices{"Flowcell"} ];
+	my $lane = $s[ $indices{"Lane"} ];
+	my $enzyme = $s[ $indices{"Enzyme"} ];
+	my $plate = $s[ $indices{"LibraryPlate"} ];
+	
+	my $info = $flowcell . "_" . $lane . "_" . $plate ;
+	
+	$info2enzyme{$info} = $enzyme;
 
 } close( 'KEY' );
 
-my @plates = keys %plate2enzyme;
+if ( ! -e "$out_dir" )	{
+		system("mkdir $out_dir");
+}
+
+my @plates = keys %info2enzyme;
+
+
 
 foreach my $p ( @plates )	{
 		
-	opendir(DIR, "GBSX_fastqs/$p" ) or die "cannot open GBSX_fastqs/$p";
+	opendir(DIR, "$in_dir/$p" ) or die "cannot open $in_dir/$p";
 	my @f = grep(/.fastq.gz$/,readdir(DIR));
 	
-	if ( -e "GBSX_fastqs_trimmed/$p" )	{
-	} else {
-		system("mkdir GBSX_fastqs_trimmed/$p");
+	if ( ! -e "$out_dir/$p" )	{
+		system("mkdir $out_dir/$p");
 	}
 
-	my $enzyme = $plate2enzyme{$p};
-
+	my $enzyme = $info2enzyme{$p};
 	my $full = $full{$enzyme};	
-
 	my $full_length = length($full);
 
 	foreach my $f ( @f )	{
@@ -47,7 +84,7 @@ foreach my $p ( @plates )	{
 		my $trimmed = $f;
 		$trimmed =~ s/.R1.fastq.gz/.trimmed.fastq.gz/;	
 				
-		print "/home/smyles/myles_lab/bin/bbmap_35.82/bbduk.sh -Xmx1g in=GBSX_fastqs/$p/$f out=GBSX_fastqs_trimmed/$p/$trimmed k=$full_length literal=\"$full\" rcomp=f copyundefined ktrim=r minlength=30 trimq=20 qtrim=r\n";
+		print "$bbmap -Xmx1g in=$in_dir/$p/$f out=$out_dir/$p/$trimmed k=$full_length literal=\"$full\" rcomp=f copyundefined ktrim=r minlength=30 trimq=20 qtrim=r\n";
 	
 	}
 }
--- merge_ABC_domestica_ind_fastqs.pl
+++ merge_ABC_domestica_ind_fastqs.pl
@@ -1,71 +1,80 @@
 use strict;
 use warnings;
 
+if ( scalar @ARGV != 3 )	{	
+
+	die "usage: perl merge_ABC_ind_fastqs.pl (directory with trimmed fastqs) (keyfile) (output directory)\n";
+	
+}
+
+my $in_dir = $ARGV[0];
+my $key = $ARGV[1];
+my $out_dir = $ARGV[2];
+
+if ( ! -e $out_dir )	{	system( "mkdir $out_dir" )	}
+
 my $lc = 0;
 
-my %plate2file = ();
 my %apple_id = ();
 
+# indices of columns of interest, figured out by parsing header line
+my %indices = ();
+$indices{"DNASample"} = undef;
+$indices{"LibraryPlate"} = undef;
+$indices{"Row"} = undef;
+$indices{"Col"} = undef;
+$indices{"Flowcell"} = undef;
+$indices{"Lane"} = undef;
+$indices{"apple_id"} = undef;
 
-open( 'BARCODE_KEY' , '</home/smyles/myles_lab/apple_GBS/Tassel5_realign/keyfiles/20151116_gbs_keyfile_usda_abc_cet_barcode_splitter.txt') or die "cant open BARCODE_KEY /home/smyles/myles_lab/apple_GBS/barcodeSplit_fastqs/20151116_gbs_keyfile_usda_abc_cet_barcode_splitter.txt\n";
-while( <BARCODE_KEY> )	{
-	
-	if ( $lc == 0 )	{	++$lc;	next	};
-	
-	my @s = split( '\s+' , $_ );
-	
-	my $plate = $s[4];
-	my $well  = $s[5] . $s[6];
-	my $dnasample = $s[3];
-	
-	my $fastq = "/home/smyles/myles_lab/apple_GBS/GBSX_split_fastqs/GBSX_fastqs_trimmed/".$plate."/".$plate."_".$well."_".$dnasample.".trimmed.fastq.gz";
-	
-	my $location = $plate."_".$well;
-	
-	$plate2file{$location} = $fastq;
-	
-} close( 'BARCODE_KEY' );
-
-$lc = 0;
-
-open ( 'KEY' , '</home/smyles/myles_lab/apple_GBS/Tassel5_realign/20151116_gbs_keyfile_usda_abc_cet.txt' ) or die "can't open KEY /home/smyles/myles_lab/apple_GBS/Tassel5_realign/20151116_gbs_keyfile_usda_abc_cet.txt\n";
+open ( 'KEY' , '<' , $key ) or die "can't open KEY $key\n";
 while( <KEY> )	{
-		
-	if ( $lc == 0 )	{	++$lc;	next	}
 		
 	my @s = split( '[\t\n]' , $_ );
 	
-	my $num_col = scalar @s;
+	# skip first line
+	if ( $lc == 0 )	{
+		++$lc; 
+		
+		### header line, which can be used to figure out indices of interest
+		my $i = 0;
+		
+		foreach my $s ( @s )	{		
+
+			if ( exists $indices{$s} )	{
+				$indices{$s} = $i;
+			} else {}
+
+			++$i;
+		}	
+
+		next;
+	}	
+
+	my $plate = $s[ $indices{"LibraryPlate"} ] ;
+	my $Row = $s[ $indices{"Row"} ] ;
+	my $Col = $s[ $indices{"Col"} ] ;
+
 	
-	if ( scalar @s == 26 )	{
-		push ( @s , "NA" );
-	} elsif ( scalar @s == 25 )	{
-		push ( @s , ("NA", "NA") );
-	} elsif ( $num_col != 27 )	{
-		die "num col is $num_col:\n\n$_\n\n";
-	}
+	my $DNASample = $s[ $indices{"DNASample"} ] ;
+
+	my $flowcell = $s[ $indices{"Flowcell"} ] ;
+	my $lane = $s[ $indices{"Lane"} ] ;
+
+	### make sure that Col is 2 characters (so add 0 if only one digit)
+	if ( length( $Col ) == 1 )	{	$Col = "0" . $Col 	} 
+	my $well = $Row . $Col;
 	
-	my $apple_id = $s[$#s - 2];
-	$apple_id =~ s/\s//g;
-	$s[18] =~ s/\s//g;
+	my $fastq = "$in_dir/$flowcell"."_"."$lane"."_"."$plate"."/".$plate."_".$well."_".$DNASample.".trimmed.fastq.gz";
 	
-	if ( $s[18] ne "domestica" )	{	next	}
 	
+	my $apple_id = $s[ $indices{"apple_id"} ];
+
 	if ( ( $apple_id =~ m/\S/ ) and ( $apple_id ne "NA" ) )	{
 		
-		my $plate = $s[4];
-		my $well = $s[10];
-		
-		$plate =~ s/\s//g;
-		$well  =~ s/\s//g;
-	
 		if ( ! exists $apple_id{$apple_id} )	{
 			$apple_id{$apple_id} = {};
 		}
-		
-		my $location = $plate."_".$well;
-		
-		my $fastq = $plate2file{$location};
 		
 		$apple_id{$apple_id}{$fastq} = ""
 		
@@ -73,11 +82,12 @@ while( <KEY> )	{
 	
 } close( 'KEY' );
 
+	
 foreach my $id ( keys %apple_id )	{
 
 	my @fastqs = keys %{$apple_id{$id}};
 
-	print "cat @fastqs > /home/smyles/myles_lab/apple_GBS/ABC_domestica_ind_fastqs/$id".".fastq.gz\n";
+	print "cat @fastqs > $out_dir/$id".".fastq.gz\n";
 
 }
 
