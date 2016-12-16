07a0ab3ad1a1c7bd5b935f09313f8f499b7a0501
Gavin Douglas <gavin@kronos.pharmacology.dal.ca>
2016-12-16 11:38:51 -0400

    updated command-line inputs for bwa_commands.pl

--- commands/bwa_commands.pl
+++ commands/bwa_commands.pl
@@ -1,11 +1,13 @@
 use strict;
 use warnings;
 
-if ( ! exists $ARGV[0] )	{	die "(dir containing gzipped fastqs)\n";	}
+if ( scalar @ARGV != 3 )	{	die "(dir containing gzipped fastqs) (path to reference fasta) ([aln|samse])\n";	}
 
 opendir(DIR, $ARGV[0]) or die "cannot open $ARGV[0]\n";
 my @fastq = grep(/fastq.gz/,readdir(DIR) );
 
+my $ref = $ARGV[1];
+my $type = $ARGV[2];
 
 foreach my $f ( @fastq )	{
 
@@ -21,14 +23,12 @@ foreach my $f ( @fastq )	{
 	my $samse_out = "./bwa_aligned/$base" . ".sam";
 	my $samse_log = "./log/$base" . "_samse.log";
 
-	my $bwa_aln = "bwa aln -t 6 /home/smyles/myles_lab/apple_GBS/MalDom_1.0/GDR_MalDom_1.0/Malus_x_domestica.v1.0-primary.pseudo_plus_Unass_UI.fa $gzipped >$aln_out 2>$aln_log"; 
-	my $bwa_samse = "bwa samse /home/smyles/myles_lab/apple_GBS/MalDom_1.0/GDR_MalDom_1.0/Malus_x_domestica.v1.0-primary.pseudo_plus_Unass_UI.fa $aln_out $gzipped > $samse_out 2>$samse_log";
+	my $bwa_aln = "bwa aln -t 6 $ref $gzipped >$aln_out 2>$aln_log"; 
+	my $bwa_samse = "bwa samse $ref $aln_out $gzipped > $samse_out 2>$samse_log";
 
-#	print "echo \"$bwa_aln\"\n";
-#	print "$bwa_aln\n";
-
-	#print "echo \"$bwa_samse\"\n";
-	print "$bwa_samse\n";
-
-#	print "\n\n\n";
+	if ( $type eq "aln" ){
+		print "$bwa_aln\n";
+	} elsif( $type eq "samse" ) {
+		print "$bwa_samse\n";
+	} else { die "third argument needs to be either aln or samse instead of $type\n";	}
 }
